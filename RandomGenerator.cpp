c4c033ba51f2cf0db996574f5c10797f81af07b4
yauhofung <yauho.fung@student.mec.cuny.edu>
2019-12-22 14:24:55 -0500

    Updated RandomGenerator

--- RandomGenerator.cpp
+++ RandomGenerator.cpp
@@ -1,48 +1,60 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
+#include <sstream>
 using namespace std;
 
 int main()
-{       srand(time(NULL));
+{	srand(time(NULL));
 
-        bool loop=true;
 	int selection;
 
+	bool loop=true;
 	while(loop)
-	{	cout<<"Select your random generation:\n"; //selection menu
-	        cout<<"(1) Matrix Digits\n";
-		cout<<"(2) Matrix Characters\n";
-		cout<<"(3) Chaotic Digits\n";
-		cout<<"(4) Chaotic Characters\n";
-		cout<<"NOTE: To stop, press Ctrl+C.\n";
+	{	cout<<"Select your random generation:\n";
+		cout<<"(1) Single Digit\n";
+		cout<<"(2) Single Character\n";
+		cout<<"(3) Matrix Digits\n";
+		cout<<"(4) Matrix Characters\n";
+		cout<<"(5) Chaotic Digits\n";
+		cout<<"(6) Chaotic Characters\n";
+		cout<<"To terminate, press Ctrl+C. ";
 		cin>>selection;
 
-		if(selection==1) //matrix digits
-		{	while(loop)
-	        	{       if(rand()%6==0) //1-in-6 chance of printing a space
-				{	cout<<' ';}
-				else
-				{	cout<<rand()%10;}
-				cout<< "	";}}
-		else if(selection==2) //matrix characters
+		if(selection==1) //single digit
+		{	cout<<rand()%10<<"\n";} //0 to 9
+
+		else if(selection==2) //single character
+		{	cout<<char(rand()%95+32)<<"\n";} //32 to 126
+
+		else if(selection==3) //matrix digits
 		{	while(loop)
 			{	if(rand()%6==0) //1-in-6 chance of printing a space
 				{	cout<<' ';}
 				else
-				{	cout<<char(rand()%94+33);}
+				{	cout<<rand()%10;} //0 to 9
 				cout<< "	";}}
 
-		else if(selection==3) //chaotic digits
+		else if(selection==4) //matrix characters
 		{	while(loop)
-	                {       cout<<rand()%10;}}
-		else if(selection==4) //chaotic characters
+			{	if(rand()%6==0) //1-in-6 chance of printing a space
+				{	cout<<' ';}
+				else
+				{	cout<<char(rand()%94+33);} //33 to 126
+				cout<< "	";}}
+
+		else if(selection==5) //chaotic digits
 		{	while(loop)
-        	        {       cout<<char(rand()%95+32);}}
+			{	cout<<rand()%10;}} //0 to 9
+
+		else if(selection==6) //chaotic characters
+		{	while(loop)
+			{	cout<<char(rand()%95+32);}} //32 to 126
 
 		else //invalid input
-		{	throw "Invalid selection";}}
+		{	cout<<"Invalid selection.\n";}}
 
 	return 0;}
 
-//ALWAYS DOUBLE CHECK ALL COUT INDENTATIONS BEFORE SAVING!!!
+//ENSURE ALL COUT INDENTATIONS ARE AT MAX LENGTH BEFORE SAVING!!!
